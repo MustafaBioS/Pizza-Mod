89ab7c1137ee22ac860492b9a96bb5a0b3427efb
MustafaBioS <bioshpere.voices@gmail.com>
2025-09-30 19:40:58 +0300

    setup

---
+++ src/main/java/net/mustafahany/pizzamod/PizzaModClient.java
@@ -0,0 +1,12 @@
+package net.mustafahany.pizzamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PizzaModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
