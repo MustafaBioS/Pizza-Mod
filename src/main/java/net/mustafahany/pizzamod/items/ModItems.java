439573fc4972a6bbd683e40e662b4c76c4297b0f
MustafaBioS <bioshpere.voices@gmail.com>
2025-10-02 04:53:22 +0300

    m

--- src/main/java/net/mustafahany/pizzamod/items/ModItems.java
+++ src/main/java/net/mustafahany/pizzamod/items/ModItems.java
@@ -12,27 +12,26 @@ import net.mustafahany.pizzamod.PizzaMod;
 
 public class ModItems {
     public static Item DOUGH;
+    public static Item TOMATO;
 
     private static Item registerItem(String name, Item.Settings baseSettings) {
-        // Create Identifier & RegistryKey for the item
         Identifier id = Identifier.of(PizzaMod.MOD_ID, name);
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
 
-        // Attach the registry key to settings so Item.Settings has the id/translation key
         Item.Settings settingsWithKey = baseSettings.registryKey(key);
 
-        // Register using the RegistryKey overload
         return Registry.register(Registries.ITEM, key, new Item(settingsWithKey));
     }
 
     public static void registerModItems() {
         PizzaMod.LOGGER.info("Registering Mod Items for " + PizzaMod.MOD_ID);
 
-        // Create + register item here (do NOT initialize as a static final at class-load time)
         DOUGH = registerItem("dough", new Item.Settings());
+        TOMATO = registerItem("tomato", new Item.Settings());
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(DOUGH);
+            entries.add(TOMATO);
         });
     }
 }
