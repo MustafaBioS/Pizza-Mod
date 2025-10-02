f1ffeff9e566fbfbb2aafb9bae157f5fb6a24692
MustafaBioS <bioshpere.voices@gmail.com>
2025-10-02 21:56:53 +0300

    pizza cutter problems

--- src/main/java/net/mustafahany2/pizzamod/items/ModItems.java
+++ src/main/java/net/mustafahany2/pizzamod/items/ModItems.java
@@ -3,6 +3,7 @@ package net.mustafahany2.pizzamod.items;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -14,6 +15,8 @@ import net.mustafahany2.pizzamod.block.ModBlocks;
 public class ModItems {
     public static final Item DOUGH = registerItem("dough", new Item(new Item.Settings()));
     public static final Item TOMATO = registerItem("tomato", new Item(new Item.Settings()));
+    public static final Item PIZZA_CUTTER = registerItem("pizza_cutter", new SwordItem(ModToolMaterials.PIZZA_CUTTER,
+            new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.PIZZA_CUTTER, 3, -2.4F))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(PizzaMod.MOD_ID, name), item);
--- src/main/java/net/mustafahany2/pizzamod/util/ModTags.java
+++ src/main/java/net/mustafahany2/pizzamod/util/ModTags.java
@@ -1,4 +1,26 @@
 package net.mustafahany2.pizzamod.util;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.mustafahany2.pizzamod.PizzaMod;
+
 public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_PIZZA_TOOL = createTag("needs_pizza_tool");
+        public static final TagKey<Block> INCORRECT_FOR_PIZZA_TOOL = createTag("incorrect_for_pizza_tool");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(PizzaMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(PizzaMod.MOD_ID, name));
+        }
+    }
 }
