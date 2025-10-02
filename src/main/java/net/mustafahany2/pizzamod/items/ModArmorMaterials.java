1869df7facc842723d027502d0a9e8093f7bd3e5
MustafaBioS <bioshpere.voices@gmail.com>
2025-10-03 01:40:55 +0300

    armor

--- src/main/java/net/mustafahany2/pizzamod/items/ModArmorMaterials.java
+++ src/main/java/net/mustafahany2/pizzamod/items/ModArmorMaterials.java
@@ -1,16 +1,36 @@
 package net.mustafahany2.pizzamod.items;
 
+import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Util;
 import net.mustafahany2.pizzamod.PizzaMod;
 import net.minecraft.util.Identifier;
 
 public class ModArmorMaterials {
 
+    public static final RegistryEntry<ArmorMaterial> PIZZA_ARMOR_MATERIAL = registerArmorMaterial("pizza",
+            () -> new ArmorMaterial(Util.make(new EnumMap(ArmorItem.Type.class), map -> {
+                map.put(ArmorItem.Type.BOOTS, 2);
+                map.put(ArmorItem.Type.LEGGINGS, 4);
+                map.put(ArmorItem.Type.CHESTPLATE, 6);
+                map.put(ArmorItem.Type.HELMET, 2);
+                map.put(ArmorItem.Type.BODY, 4);
+            }), 20, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, () -> Ingredient.ofItems(ModItems.DOUGH),
+                    List.of(new ArmorMaterial.Layer(Identifier.of(PizzaMod.MOD_ID, "pizza"))), 0, 0));
 
+    public static RegistryEntry<ArmorMaterial> registerArmorMaterial(String name, Supplier<ArmorMaterial> material) {
+        return Registry.registerReference(Registries.ARMOR_MATERIAL, Identifier.of(PizzaMod.MOD_ID, name), material.get());
+    }
 }
--- src/main/java/net/mustafahany2/pizzamod/items/ModItems.java
+++ src/main/java/net/mustafahany2/pizzamod/items/ModItems.java
@@ -1,6 +1,7 @@
 package net.mustafahany2.pizzamod.items;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.SwordItem;
@@ -11,12 +12,30 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.mustafahany2.pizzamod.PizzaMod;
 import net.mustafahany2.pizzamod.block.ModBlocks;
+import net.mustafahany2.pizzamod.util.ModTags;
 
 public class ModItems {
     public static final Item DOUGH = registerItem("dough", new Item(new Item.Settings()));
     public static final Item TOMATO = registerItem("tomato", new Item(new Item.Settings()));
     public static final Item PIZZA_CUTTER = registerItem("pizza_cutter", new SwordItem(ModToolMaterials.PIZZA_CUTTER,
             new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.PIZZA_CUTTER, 3, -2.4F))));
+
+    public static final Item PIZZA_HELMET = registerItem("pizza_helmet", new ArmorItem(
+            ModArmorMaterials.PIZZA_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings()
+            .maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15))));
+
+    public static final Item PIZZA_CHESTPLATE = registerItem("pizza_chestplate", new ArmorItem(
+            ModArmorMaterials.PIZZA_ARMOR_MATERIAL, ArmorItem.Type.CHESTPLATE, new Item.Settings()
+            .maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(15))));
+
+    public static final Item PIZZA_LEGGINGS = registerItem("pizza_leggings", new ArmorItem(
+            ModArmorMaterials.PIZZA_ARMOR_MATERIAL, ArmorItem.Type.LEGGINGS, new Item.Settings()
+            .maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(15))));
+
+
+    public static final Item PIZZA_BOOTS = registerItem("pizza_boots", new ArmorItem(
+            ModArmorMaterials.PIZZA_ARMOR_MATERIAL, ArmorItem.Type.BOOTS, new Item.Settings()
+            .maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(15))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(PizzaMod.MOD_ID, name), item);
@@ -29,6 +48,13 @@ public class ModItems {
             entries.add(DOUGH);
             entries.add(TOMATO);
             entries.add(ModBlocks.PIZZA);
+            entries.add(PIZZA_CUTTER);
+
+            entries.add(PIZZA_HELMET);
+            entries.add(PIZZA_CHESTPLATE);
+            entries.add(PIZZA_LEGGINGS);
+            entries.add(PIZZA_BOOTS);
+
         });
     }
 }
